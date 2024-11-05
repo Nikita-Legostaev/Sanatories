6e44484e4a6f98247765e5907bef3e71dc6d28a7
Nikita <nikitooo111333222@gmail.com>
2024-11-05 19:03:50 +0300

    update date

--- app/models/staff/models.py
+++ app/models/staff/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 class Staff(Base):
     __tablename__='staff'
 
-    id: Mapped[int]=mapped_column(primary_key=True)
+    id: Mapped[int]=mapped_column(primary_key=True, autoincrement=True)
     fullname: Mapped[str]
     position: Mapped[str]
     phone: Mapped[str]
